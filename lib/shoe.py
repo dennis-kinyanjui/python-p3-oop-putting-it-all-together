ee89182af4eaff7957298b646ddf3114230c8957
Dennis <dennis.kinyanjui@student.moringaschool.com>
2024-05-27 08:51:31 +0300

    complete lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,59 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.condition = "New"  # Default condition when a shoe is new
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
+import io
+import sys
+
+class TestShoe:
+    '''Shoe in shoe.py'''
+
+    def test_has_brand_and_size(self):
+        '''has the brand and size passed to __init__, and values can be set to new instance.'''
+        stan_smith = Shoe("Adidas", 9)
+        assert(stan_smith.brand == "Adidas")
+        assert(stan_smith.size == 9)
+
+    def test_requires_int_size(self):
+        '''prints "size must be an integer" if size is not an integer.'''
+        stan_smith = Shoe("Adidas", 9)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        stan_smith.size = "not an integer"
+        sys.stdout = sys.__stdout__
+        assert captured_out.getvalue() == "size must be an integer\n"
+
+    def test_can_cobble(self):
+        '''says that the shoe has been repaired.'''
+        stan_smith = Shoe("Adidas", 9)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        stan_smith.cobble()
+        sys.stdout = sys.__stdout__
+        assert captured_out.getvalue() == "Your shoe is as good as new!\n"
+    
+    def test_cobble_makes_new(self):
+        '''creates an attribute on the instance called 'condition' and set equal to 'New' after repair.'''
+        stan_smith = Shoe("Adidas", 9)
+        stan_smith.cobble()
+        assert stan_smith.condition == "New"
